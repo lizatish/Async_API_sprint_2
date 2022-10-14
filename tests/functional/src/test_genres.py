8ccc2b91e2674d7258c6c478f307449f66b50b65
Oleg <oleg.rubtsov99@gmail.com>
2022-10-14 17:42:27 +0400

    add genres tests

--- tests/functional/src/test_genres.py
+++ tests/functional/src/test_genres.py
@@ -1,9 +1,61 @@
 import pytest
+from aioredis import Redis
 from httpx import AsyncClient
+from tests.functional.config import get_settings
+from tests.functional.testdata.genres import (test_data_for_genre,
+                                              test_data_for_genres_list)
+
+conf = get_settings()
 
 
+@pytest.mark.parametrize(
+    'id_genre, expected_body, expected_answer', test_data_for_genre
+)
 @pytest.mark.asyncio
-async def test_get_genres_list(genres_api_client: AsyncClient):
-    response = await genres_api_client.get("/api/v1/genres/")
+async def test_get_genre_by_id(
+    film_works_api_client: AsyncClient, redis_pool: Redis, id_genre: str, expected_body: dict, expected_answer: dict
+) -> None:
+    """
+    Тест для подробного просмотра genre.
 
-    assert response.status_code == 200
+    Проверяет:
+    - наличие жанра с указанным id
+    - наличие жанра в кэше
+    - соответствие жанра ожидаемому значению
+    """
+    response = await film_works_api_client.get(f"/api/v1/genres/{id_genre}")
+    response_body = response.json()
+    redis_data = await redis_pool.get(id_genre)
+    if expected_answer['redis_data']:
+        assert redis_data
+    else:
+        assert not redis_data
+    assert response.status_code == expected_answer['status']
+    assert response_body == expected_body
+
+
+@pytest.mark.parametrize(
+    'expected_answer', test_data_for_genres_list
+)
+@pytest.mark.asyncio
+async def test_get_genres_list(
+    film_works_api_client: AsyncClient, redis_pool: Redis, expected_answer: dict
+) -> None:
+    """
+    Тест для получения всех жанров.
+
+    Проверяет:
+    - наличие жанров в кэше
+    - количество элементов в ответе на запрос
+    - количество элементов в кэше
+    """
+    response = await film_works_api_client.get(f"/api/v1/genres/")
+    response_body = response.json()
+    redis_data = await redis_pool.lrange(f'{conf.BASE_URL}/api/v1/genres/', 0, -1)
+    if expected_answer['redis_data']:
+        assert redis_data
+    else:
+        assert not redis_data
+    assert len(redis_data) == expected_answer['redis_length']
+    assert response.status_code == expected_answer['status']
+    assert len(response_body) == expected_answer['response_length']
--- tests/functional/testdata/genres.py
+++ tests/functional/testdata/genres.py
@@ -104,3 +104,35 @@ es_genres_data = [
         'name': 'News',
     },
 ]
+
+
+test_data_for_genre = [
+    (
+        "120a21cf-9097-479e-904a-13dd7198c1dd",
+        {
+            "uuid": "120a21cf-9097-479e-904a-13dd7198c1dd",
+            "name": "Adventure"
+        },
+        {'status': 200, 'redis_data': True}
+    ),
+    (
+        "f24fd632-b1a5-4273-a835-0119bd12f829",
+        {
+            "uuid": "f24fd632-b1a5-4273-a835-0119bd12f829",
+            "name": "News"
+        },
+        {'status': 200, 'redis_data': True}
+    ),
+    (
+        'b1a2aae8-5c9e-4583-b89e-883c0d0c969M',
+        {'detail': "Genre not found"},
+        {'status': 404, 'redis_data': False}
+    )
+]
+
+test_data_for_genres_list = [
+    (
+        {'status': 200, 'redis_data': True, 'response_length': 26, 'redis_length': 26}
+    ),
+
+]
